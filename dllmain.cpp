b6644535a41740feb165e1c9b69bd129cd839e94
knoxfighter <knoxfighter@asdff45.de>
2021-03-14 15:05:55 +0100

    dumped new version

--- dllmain.cpp
+++ dllmain.cpp
@@ -391,7 +391,7 @@ arcdps_exports* mod_init() {
 
 	arc_exports.imguivers = IMGUI_VERSION_NUM;
 	arc_exports.out_name = "killproof.me";
-	arc_exports.out_build = "2.1.1";
+	arc_exports.out_build = "2.2.0";
 
 	if (loading_successful) {
 		/* for arcdps */
